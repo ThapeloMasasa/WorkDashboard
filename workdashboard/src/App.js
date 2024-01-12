6ee03b24c1f834a1b8f0d92d8e7b875fe69ccebb
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2024-01-12 05:51:45 +0000

    added side Bar

--- workdashboard/src/App.js
+++ workdashboard/src/App.js
@@ -4,6 +4,7 @@ import { FiSettings } from 'react-icons/fi';
 import {TooltipComponent} from '@syncfusion/ej2-react-popups';
 import './App.css';
 const App = () => {
+  const ActiveMenu = false;
   return (
     <div>
       <BrowserRouter>
@@ -11,10 +12,51 @@ const App = () => {
          <div className="fixed right-4 bottom-4" style={{zIndex: '1000'}}>
          <TooltipComponent content= "Settings" position="Top" >
                 <button
-                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue'}}>
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: 'blue', borderRadius: '50%'}}>
                     <FiSettings />
                 </button>
          </TooltipComponent>
+         </div>
+         {
+          ActiveMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-bg bg-white"> 
+              SideBar
+            </div>
+          ):(
+            <div className='w-0 dark:bg-secondary-dark-bg'>
+            SideBar w-0
+            </div>
+          )
+         }
+         <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-ful ${ActiveMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full"> 
+                NavBar
+            </div>
+         </div>
+         <div>
+            <Routes>
+              // DashBoard
+              <Route path="/" element="Ecommerce" />
+              <Route path="/ecommerce" element="Ecommerce" />
+              // Pages
+              <Route path="/orders" element="Orders" />
+              <Route path="/employees" element="Employees" />
+              <Route path="/customers" element="Customers" />
+              // Apps
+              <Route path="/kanban" element="Kanban" />
+              <Route path="/editor" element="Editor" />
+              <Route path="/calendar" element="Calendar" />
+              <Route path="/color-picker" element="colorPicker" />
+              // Charts
+              <Route path="/line" element="Line" />
+              <Route path="/area" element="Area" />
+              <Route path="/bar" element="Bar" />
+              <Route path="/pie" element="Pie" />
+              <Route path="/financial" element="Financial" />
+              <Route path="/color-mapping" element="ColorMapping" />
+              <Route path="/pyramid" element="Pyramid" />
+              <Route path="/stacked" element="Stacked" />
+            </Routes>
          </div>
         </div>
       </BrowserRouter>
