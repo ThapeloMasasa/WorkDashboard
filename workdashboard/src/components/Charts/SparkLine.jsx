227589f9328e11c38db9d052d81f130fc5ed1158
Masasa <73863365+ThapeloMasasa@users.noreply.github.com>
2024-01-17 20:05:06 +0000

    Finshed App

--- workdashboard/src/components/Charts/SparkLine.jsx
+++ workdashboard/src/components/Charts/SparkLine.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import {SparklineComponent, Inject, SparklineTooltip} from '@syncfusion/ej2-react-charts';
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
 
-const SparkLine = ({id, height, width, color, data, type, currentColor}) => {
-  return (
-    <SparklineComponent id={id} 
-                        height={height} 
-                        width={width} 
-                        dataSource={data} 
-                        xName='xval' 
-                        fill={color}
-                        valueType='Numeric'
-                        lineWidth={2}
-                        border={{color: currentColor, width: 2}}
-                        yName='yval' 
-                        tooltipSettings={{
-                          visible: true, format: '${xval} : ${yval}',
-                          trackLineSettings:{
-                            visible: true
-                          }}}
-                        type={type}>
-                        <Inject services={[SparklineTooltip]} />
-</SparklineComponent>
-  )
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        tooltipSettings={{
+          visible: true,
+          // eslint-disable-next-line no-template-curly-in-string
+          format: '${x} : data ${yval}',
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+      >
+        <Inject services={[SparklineTooltip]} />
+      </SparklineComponent>
+    );
+  }
 }
 
-export default SparkLine
+export default SparkLine;
